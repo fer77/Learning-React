b998467523ba19d90775453cc33459b3c5f57040
Fernando Fernandez <ffmp777@yahoo.com>
2016-04-26 15:02:09 -0600

    added video list

---
+++ src/components/video_list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+    //we'll need to loop over the array we get from API. Try not use a 'for' loop, instead use a built in iterator like '.map'
+    const videoItems = props.videos.map((video) => { 
+        return <VideoListItem video={video} />
+    });
+    
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; //If it is an installed file (npm, library) we need to just name the package.
 import SearchBar from './components/search_bar'; //If it is a file we created use the relative path.
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDm8la3kNoy3e-MgO03WF-qegcoo2qdtgk';
 
 //1.  Create a new component, that will produce some HTML
-const App = () => {
-    return (
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); //when key and value are identical (the same name) condense like this: {name of variable}.
+    });
+}
+    
+    render() {
+        return (
         <div>
             <SearchBar />
+            <VideoList videos={this.state.videos} />
         </div>
- );
+  );
+ }
 }
 
 //2.  Put this component's generated HTML on the DOM (page)
