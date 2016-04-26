91fcef6886261fe16a1376ca5aa4e3bf8b57bbbb
Fernando Fernandez <ffmp777@yahoo.com>
2016-04-26 14:10:52 -0600

    initialized and set state for searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react'; //Even tho we are not making a call to React, we still need this if we are using JSX.
 
-const SearchBar = () => {
-    return <input />; //React.createElement
-};
+class SearchBar extends Component { //this gives SearchBar functionality from React.Component.
+    //initializing state.
+    constructor(props) {
+        //this calls the parent method.
+        super(props);
+        
+        //To initialize state crate a new obj. and assign it to this.state.  Term is the property that records the value of the input.
+        //Only inside of the constructor function we can change our state like this.
+        //Everywhere else in our components we use the method this.setState.
+        this.state = { term: '' };
+    }
+    
+    render() { // Every React 'class' component must have a 'render method'.
+        return (
+        <div>
+            <input //Becomes a controlled component by using value...
+                value={this.state.term}
+                onChange={ event => this.setState({ term: event.target.value }) } />
+        </div>
+        );
+    }
+}
 
-export default SearchBar;
+export default SearchBar; //export so we can access this module in our index.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; //If it is an installed file (npm) we need to just name the package.
-import SearchBar from './components/search_bar'; //If it is a file we created use relative path.
+import ReactDOM from 'react-dom'; //If it is an installed file (npm, library) we need to just name the package.
+import SearchBar from './components/search_bar'; //If it is a file we created use the relative path.
 
 const API_KEY = 'AIzaSyDm8la3kNoy3e-MgO03WF-qegcoo2qdtgk';
 
 //1.  Create a new component, that will produce some HTML
-
 const App = () => {
     return (
         <div>
