4a05b1395e93f48cabe493f876c8ada4e33e1196
Fernando Fernandez <ffmp777@yahoo.com>
2016-04-27 10:01:14 -0600

    adding videos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; //If it is an installed file (npm, library) we need to just name the package.
 import SearchBar from './components/search_bar'; //If it is a file we created use the relative path.
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDm8la3kNoy3e-MgO03WF-qegcoo2qdtgk';
@@ -11,10 +12,16 @@ class App extends Component {
     constructor(props) {
         super(props);
         
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        }; //Selecting a video >> pass it to VideoDetail
         
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }); //when key and value are identical (the same name) condense entry like this: {name of variable}.
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            }); //Kickoff request for the videos and change state again.
     });
 }
     
@@ -22,6 +29,7 @@ class App extends Component {
         return (
         <div>
             <SearchBar />
+            <VideoDetail video={ this.state.selectedVideo } />
             <VideoList videos={this.state.videos} />
         </div>
   );
